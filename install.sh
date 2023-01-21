95a888b109986f3eca2f5bf2240b3e38e328bc3a
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-01-21 22:38:09 +0200

    Add terminal, shell, plasma, latte, wallpapers

--- install.sh
+++ install.sh
@@ -17,3 +17,34 @@ ln -s /home/$USR/.dotfiles/icons/ $DIR
 DIR=/home/$USR/.local/share/color-schemes
 [ -d "$DIR" ] && rm -R $DIR
 ln -s /home/$USR/.dotfiles/color-schemes/ $DIR
+
+# ---------------------- SPICETIFY ----------------------------
+DIR=/home/$USR/.spicetify
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/.spicetify/ $DIR
+
+# ---------------------- WALLPAPERS ----------------------------
+DIR=/home/$USR/wallpapers
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/wallpapers/ $DIR
+
+# ---------------------- ALACRITTY ----------------------------
+DIR=/home/$USR/.config/alacritty
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/alacritty/ $DIR
+
+# ---------------------- AUTOSTART ----------------------------
+DIR=/home/$USR/.config/autostart
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/autostart/ $DIR
+
+# ---------------------- FISH ----------------------------
+DIR=/home/$USR/.config/fish
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/fish/ $DIR
+
+# ---------------------- LATTE ----------------------------
+DIR=/home/$USR/.config/latte
+[ -d "$DIR" ] && rm -R $DIR
+ln -s /home/$USR/.dotfiles/latte/ $DIR
+
