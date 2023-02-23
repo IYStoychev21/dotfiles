d0a4ab1dd2072f33e3f6d696bdf9f3ff7d105236
IYStoychev21 <IYStoychev21@codingburgas.bg>
2023-02-23 16:18:15 +0200

    Add config file for hyprland

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,8 @@
-function print_center -a width
-    set -e argv[1] #Remove width argument
-    set -l len (string length -- "$argv")
-    if test $len -lt $width
-        set -l half (math -s 0 "($width / 2)" + "($len / 2)")
-        set -l rem (math -s 0 $width - $half)
-        printf "%*.*s%*s\n" $half $len "$argv" $rem ' '
-    else
-        printf "%*.*s\n" $width $width "$argv"
-    end
-end
-
 if status is-interactive
 
 neofetch
 starship init fish | source
 
 set fish_greeting
-
-print_center 120 "Hello there!"
 
 end
---
+++ install.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+import platform
+
+print("Enter your username: ")
+user = input()
+
+def link_to_dot_config(app):
+    if os.path.exists(f"/home/{user}/.config/{app}"):
+        subprocess.run(f"sudo rm -R /home/{user}/.config/{app}", shell=True)
+    
+    subprocess.run(f"sudo ln -s /home/{user}/dotfiles/{app} /home/{user}/.config/{app}", shell=True)
+
+# ----------------- ALL CONFINGS THAT NEED TO GO TO .CONFIG FOLDER ----------------
+apps_for_dot_config = ["alacritty", "dunst", "fish", "hypr", "waybar", "starship.toml", "wofi"]
+
+if platform.system() == "Linux":
+   for i in apps_for_dot_config:
+       link_to_dot_config(i)
+    
