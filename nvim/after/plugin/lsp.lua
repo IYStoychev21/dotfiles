9d98954f2d58c51a55c3f94f7fdcdc46ab34c90e
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-09-10 10:07:58 +0300

    Add zed config

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -4,18 +4,4 @@ lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
 end)
 
-lsp.ensure_installed({
-	"tsserver",
-	"eslint",
-	"rust_analyzer",
-	"clangd",
-	"cssls",
-	"tailwindcss",
-	"volar",
-	"dockerls",
-	"html",
-	"pyre",
-})
-
 lsp.setup()
-
